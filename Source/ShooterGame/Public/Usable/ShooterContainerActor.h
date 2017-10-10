729e458828e195d15a0c2ceaf59277ed0096575a
bpantzis <bpantzis01@gmail.com>
2017-10-10 14:40:26 -0400

    Container Actor addition

---
+++ Source/ShooterGame/Public/Usable/ShooterContainerActor.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Usable/ShooterUsableActor.h"
+#include "ShooterContainerActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERGAME_API AShooterContainerActor : public AShooterUsableActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
